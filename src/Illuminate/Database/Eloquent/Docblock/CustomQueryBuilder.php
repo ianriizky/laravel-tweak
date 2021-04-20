c91604bdc264b9382b8eee33c43bf80dc7b1b6bd
ianriizky <ian.rizkypratama@gmail.com>
2021-04-20 17:52:31 +0800

    + Finish create eloquent and query builder docblock

--- src/Illuminate/Database/Eloquent/Docblock/CustomQueryBuilder.php
+++ src/Illuminate/Database/Eloquent/Docblock/CustomQueryBuilder.php
@@ -3,14 +3,14 @@
 namespace Ianrizky\Illuminate\Database\Eloquent\Docblock;
 
 /**
- * @method static \Ianrizky\Illuminate\Database\Query\Builder whereLike(string $column, mixed $value, string $boolean = 'and')
- * @method static \Ianrizky\Illuminate\Database\Query\Builder orWhereLike(string $column, mixed $value)
- * @method static \Ianrizky\Illuminate\Database\Query\Builder whereLikeEnd(string $column, mixed $value, string $boolean = 'and')
- * @method static \Ianrizky\Illuminate\Database\Query\Builder orWhereLikeEnd(string $column, mixed $value)
- * @method static \Ianrizky\Illuminate\Database\Query\Builder whereLikeStart(string $column, mixed $value, string $boolean = 'and')
- * @method static \Ianrizky\Illuminate\Database\Query\Builder orWhereLikeStart(string $column, mixed $value)
- * @method static \Ianrizky\Illuminate\Database\Query\Builder whereLikeAny(string $column, mixed $value, string $boolean = 'and')
- * @method static \Ianrizky\Illuminate\Database\Query\Builder orWhereLikeAny(string $column, mixed $value)
+ * @method static \Illuminate\Database\Eloquent\Builder whereLike(string $column, mixed $value, string $boolean = 'and')
+ * @method static \Illuminate\Database\Eloquent\Builder orWhereLike(string $column, mixed $value)
+ * @method static \Illuminate\Database\Eloquent\Builder whereLikeEnd(string $column, mixed $value, string $boolean = 'and')
+ * @method static \Illuminate\Database\Eloquent\Builder orWhereLikeEnd(string $column, mixed $value)
+ * @method static \Illuminate\Database\Eloquent\Builder whereLikeStart(string $column, mixed $value, string $boolean = 'and')
+ * @method static \Illuminate\Database\Eloquent\Builder orWhereLikeStart(string $column, mixed $value)
+ * @method static \Illuminate\Database\Eloquent\Builder whereLikeAny(string $column, mixed $value, string $boolean = 'and')
+ * @method static \Illuminate\Database\Eloquent\Builder orWhereLikeAny(string $column, mixed $value)
  *
  * @see \Ianrizky\Illuminate\Database\Query\Builder
  */
--- src/Illuminate/Database/Eloquent/Docblock/EloquentBuilder.php
+++ src/Illuminate/Database/Eloquent/Docblock/EloquentBuilder.php
@@ -3,7 +3,7 @@
 namespace Ianrizky\Illuminate\Database\Eloquent\Docblock;
 
 /**
- * @method static \Ianrizky\Illuminate\Database\Eloquent\Model|static make(array $attributes = []) Create and return an un-saved model instance.
+ * @method static static make(array $attributes = []) Create and return an un-saved model instance.
  * @method static \Illuminate\Database\Eloquent\Builder withGlobalScope(string $identifier, \Illuminate\Database\Eloquent\Scope|\Closure $scope) Register a new global scope.
  * @method static \Illuminate\Database\Eloquent\Builder withoutGlobalScope(\Illuminate\Database\Eloquent\Scope|string $scope) Remove a registered global scope.
  * @method static \Illuminate\Database\Eloquent\Builder withoutGlobalScopes(array $scopes = null) Remove all or passed registered global scopes.
@@ -28,15 +28,15 @@ namespace Ianrizky\Illuminate\Database\Eloquent\Docblock;
  * @method static static|mixed firstOr(\Closure|array $columns = ['*'], \Closure|null $callback = null) Execute the query and get the first result or call a callback.
  * @method static mixed value(string|\Illuminate\Database\Query\Expression $column) Get a single column's value from the first result of a query.
  * @method static \Ianrizky\Illuminate\Database\Eloquent\Collection get(array|string $columns = ['*']) Execute the query as a "select" statement.
- * @method static \Ianrizky\Illuminate\Database\Eloquent\Model[]|static[] getModels(array|string $columns = ['*']) Get the hydrated models without eager loading.
+ * @method static static[] getModels(array|string $columns = ['*']) Get the hydrated models without eager loading.
  * @method static array eagerLoadRelations(array $models) Eager load the relationships for the models.
  * @method static \Illuminate\Database\Eloquent\Relations\Relation getRelation(string $name) Get the relation instance for the given relation name.
  * @method static \Illuminate\Support\LazyCollection cursor() Get a lazy collection for the given query.
  * @method static \Illuminate\Support\Collection pluck(string|\Illuminate\Database\Query\Expression $column, string|null $key = null) Get an array with the values of a given column.
  * @method static \Illuminate\Contracts\Pagination\LengthAwarePaginator paginate(int|null $perPage = null, array $columns = ['*'], string $pageName = 'page', int|null $page = null) Paginate the given query.
  * @method static \Illuminate\Contracts\Pagination\Paginator simplePaginate(int|null $perPage = null, array $columns = ['*'], string $pageName = 'page', int|null $page = null) Paginate the given query into a simple paginator.
- * @method static \Ianrizky\Illuminate\Database\Eloquent\Model|$this create(array $attributes = []) Save a new model and return the instance.
- * @method static \Ianrizky\Illuminate\Database\Eloquent\Model|$this forceCreate(array $attributes) Save a new model and return the instance. Allow mass-assignment.
+ * @method static static create(array $attributes = []) Save a new model and return the instance.
+ * @method static static forceCreate(array $attributes) Save a new model and return the instance. Allow mass-assignment.
  * @method static int update(array $values) Update a record in the database.
  * @method static int increment(string|\Illuminate\Database\Query\Expression $column, float|int $amount = 1, array $extra = []) Increment a column's value by a given amount.
  * @method static int decrement(string|\Illuminate\Database\Query\Expression $column, float|int $amount = 1, array $extra = []) Decrement a column's value by a given amount.
@@ -47,13 +47,13 @@ namespace Ianrizky\Illuminate\Database\Eloquent\Docblock;
  * @method static static applyScopes() Apply the scopes to the Eloquent builder instance and return it.
  * @method static \Illuminate\Database\Eloquent\Builder with(mixed $relations) Set the relationships that should be eager loaded.
  * @method static \Illuminate\Database\Eloquent\Builder without(mixed $relations) Prevent the specified relations from being eager loaded.
- * @method static \Ianrizky\Illuminate\Database\Eloquent\Model|static newModelInstance(array $attributes = []) Create a new instance of the model being queried.
+ * @method static static newModelInstance(array $attributes = []) Create a new instance of the model being queried.
  * @method static \Illuminate\Database\Query\Builder getQuery() Get the underlying query builder instance.
  * @method static \Illuminate\Database\Eloquent\Builder setQuery(\Illuminate\Database\Query\Builder $query) Set the underlying query builder instance.
  * @method static \Illuminate\Database\Query\Builder toBase() Get a base query builder instance.
  * @method static array getEagerLoads() Get the relationships being eagerly loaded.
  * @method static \Illuminate\Database\Eloquent\Builder setEagerLoads(array $eagerLoad) Set the relationships being eagerly loaded.
- * @method static \Illuminate\Database\Eloquent\Model|static getModel() Get the model instance being queried.
+ * @method static static getModel() Get the model instance being queried.
  * @method static \Illuminate\Database\Eloquent\Builder setModel(\Ianrizky\Illuminate\Database\Eloquent\Model $model) Set a model instance for the model being queried.
  * @method static string qualifyColumn(string|\Illuminate\Database\Query\Expression $column) Qualify the given column name by the model's table.
  * @method static \Closure getMacro(string $name) Get the given macro by name.
