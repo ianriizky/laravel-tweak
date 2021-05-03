73748982dbb6e491f4b606877bd008f271b27b09
ianriizky <ian.rizkypratama@gmail.com>
2021-05-03 14:25:54 +0800

    + Use custom eloquent and query builder as docblock definition

--- src/Illuminate/Database/Eloquent/Docblock/BuildsQueries.php
+++ src/Illuminate/Database/Eloquent/Docblock/BuildsQueries.php
@@ -10,8 +10,8 @@ namespace Ianrizky\Illuminate\Database\Eloquent\Docblock;
  * @method static bool chunkById(int $count, callable $callback, string|null $column = null, string|null $alias = null) Chunk the results of a query by comparing IDs.
  * @method static bool eachById(callable $callback, int $count = 1000, string|null $column = null, string|null $alias = null) Execute a callback over each item while chunking by id.
  * @method static static|null first(array|string $columns = ['*']) Execute the query and get the first result.
- * @method static mixed|\Illuminate\Database\Eloquent\Builder|\Ianrizky\Illuminate\Database\Query\Builder when(mixed $value, callable $callback, callable|null $default = null) Apply the callback's query changes if the given "value" is true.
- * @method static mixed|\Illuminate\Database\Eloquent\Builder|\Ianrizky\Illuminate\Database\Query\Builder unless(mixed $value, callable $callback, callable|null $default = null) Apply the callback's query changes if the given "value" is false.
+ * @method static mixed|\Ianrizky\Illuminate\Database\Eloquent\Builder|\Ianrizky\Illuminate\Database\Query\Builder when(mixed $value, callable $callback, callable|null $default = null) Apply the callback's query changes if the given "value" is true.
+ * @method static mixed|\Ianrizky\Illuminate\Database\Eloquent\Builder|\Ianrizky\Illuminate\Database\Query\Builder unless(mixed $value, callable $callback, callable|null $default = null) Apply the callback's query changes if the given "value" is false.
  *
  * @see \Illuminate\Database\Concerns\BuildsQueries
  */
